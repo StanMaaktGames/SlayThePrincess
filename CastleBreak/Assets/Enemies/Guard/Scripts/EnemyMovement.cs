bb2c760af369f8786361214ed5e1a3791693d579
StanM2009 <27583@edu.msa.nl>
2025-07-04 11:46:17 +0200

    Added guard AI

---
+++ CastleBreak/Assets/Enemies/Guard/Scripts/EnemyMovement.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Animator animator;
+    
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    public float speed, requiredInterest;
+    public Transform[] path;
+
+    float interest, moveVelocityFloat, idleWalkDelay;
+    int pathPointIndex = 0;
+    Vector3 moveVelocity, lastPosition;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent.SetDestination(transform.position);
+    }
+
+    void Update()
+    {
+        // animator.SetInteger("movingState", AnimateMovingState());
+        lastPosition = transform.position;
+    }
+
+    public void Idle()
+    {
+        agent.isStopped = false;
+        agent.speed = speed;
+        if (agent.remainingDistance < 1.0f)
+        {
+            if (pathPointIndex > path.Length - 2)
+            {
+                pathPointIndex = 0;
+            }
+            else
+            {
+                pathPointIndex += 1;
+            }
+            agent.SetDestination(path[pathPointIndex].position);
+        }
+
+    }
+
+    // public void Searching(Vector3 lastSeenPlayerPosition)
+    // {
+    //     agent.isStopped = false;
+    //     agent.speed = speed;
+    //     agent.SetDestination(lastSeenPlayerPosition);
+    // }
+
+    public void Chase(Vector3 lastSeenPlayerPosition)
+    {
+        agent.isStopped = false;
+        agent.speed = speed * 5;
+        agent.SetDestination(lastSeenPlayerPosition);
+    }
+
+
+
+//     Quaternion RotateTowardsPlayer()
+//     {
+//         Vector3 direction = lastSeenPlayerPosition - transform.position;
+//         Quaternion rotation = Quaternion.LookRotation(direction);
+//         return Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime);
+//     }
+
+//     int AnimateMovingState() //determine whether the idle, walking or running animation should be playing
+//     {
+//         moveVelocity = ((transform.position - lastPosition)) / Time.deltaTime;
+//         moveVelocityFloat = new Vector2(Mathf.Abs(moveVelocity.x), Mathf.Abs(moveVelocity.z)).magnitude;
+//         if (moveVelocityFloat > 0)
+//         {
+//             return 1;
+//         }
+//         return 0;
+//     }
+}
